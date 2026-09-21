70463f501618af7a31063a0e2a9562974b117428
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-09-21 11:26:27 +0700

    membuat suatu kondisi siswa dalam nilai

--- praktikum_per2.py
+++ praktikum_per2.py
@@ -15,38 +15,3 @@ else:
     print("LEBIH GIAT LAGI")
 
     
-#2. Write a PYTHON program to find largest of three numbers!
-angka1 = int(input("Masukkan angka pertama: "))
-angka2 = int(input("Masukkan angka kedua: "))
-angka3 = int(input("Masukkan angka ketiga: "))
-
-if angka1 >= angka2 and angka1 >= angka3:
-    print("Angka terbesar adalah:", angka1)
-elif angka2 >= angka1 and angka2 >= angka3:
-    print("Angka terbesar adalah:", angka2)
-else:
-    print("Angka terbesar adalah:", angka3)
-
-#3. Write a PYTHON program to print Fibonacci series up to n!
-n = int(input("Masukkan jumlah elemen Fibonacci: "))
-a, b = 0, 1
-for _ in range(n):
-    print(a, end=' ')
-    a, b = b, a + b
-
-
-#4. Write a PYTHON program to print odd numbers up to n!
-
-n = int(input("masukkan nilai n: "))
-print(f"Odd numbers up to {n}:")
-for i in range(1, n + 1, 2):
-    print(i, end=" ")
-
-
-#5. Write a PYTHON program to produce following design
-n = int(input("Masukkan nilai n: "))
-for i in range(1, n + 1):
-    for j in range(i):
-        print(i, end=" ")
-    print()
- 
