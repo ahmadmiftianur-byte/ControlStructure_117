29e2661b008a0a614e60501233178a26df23f56e
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-09-21 11:27:23 +0700

    membuat sistem tau nilai largest

--- praktikum_per2.py
+++ praktikum_per2.py
@@ -15,3 +15,15 @@ else:
     print("LEBIH GIAT LAGI")
 
     
+#2. Write a PYTHON program to find largest of three numbers!
+angka1 = int(input("Masukkan angka pertama: "))
+angka2 = int(input("Masukkan angka kedua: "))
+angka3 = int(input("Masukkan angka ketiga: "))
+
+if angka1 >= angka2 and angka1 >= angka3:
+    print("Angka terbesar adalah:", angka1)
+elif angka2 >= angka1 and angka2 >= angka3:
+    print("Angka terbesar adalah:", angka2)
+else:
+    print("Angka terbesar adalah:", angka3)
+
